cef93d1a3cd16ca688dc3abf5678258f4247258f
Phong Yang <phongyang88@hotmail.com>
2020-01-16 17:30:24 -0600

    added logic behind fouls, strikes, balls to fit rules specified

--- web-application-testing-baseball-scoreboard/src/App.js
+++ web-application-testing-baseball-scoreboard/src/App.js
@@ -4,34 +4,56 @@ import Scoreboard from "./components/Scoreboard";
 import './App.css';
 
 function App() {
+  const initialMessage = "A new player at bat.";
   const [balls, setBalls] = useState(0);
   const [strikes, setStrikes] = useState(0);
   const [fouls, setFouls] = useState(0);
+  const [message, setMessage] = useState(initialMessage);
 
   const increaseBall = () => {
-    setBalls(balls+1);
-    console.log("increase ball button clicked")
+    if (balls <3){
+      setBalls(balls+1);
+      setMessage("Ball!");
+    }else {
+      clearBoard();
+      setMessage("ball 4! player moved to first base. next hitter up to bat!")
+    }
   }
 
   const increaseStrike = () => {
-    setStrikes(strikes+1);
-    console.log("increase strike button clicked")
+    if (strikes <2){
+      setMessage("Strike!");
+      setStrikes(strikes+1);
+    }else {
+      clearBoard();
+      setMessage("Strike 3! Player struck out! next hitter up to bat!")
+    }
   }
 
   const increaseFoul = () => {
     setFouls(fouls+1);
-    console.log("increase foul button clicked")
+    if (strikes<2){
+      increaseStrike();
+    }
+    setMessage("Foul!")
   }
 
-  const hit = () => {
+  const clearBoard = () => {
     setBalls(0);
     setFouls(0);
     setStrikes(0);
-    console.log("hit button clicked. balls, fouls, and strikes reset to 0")
+  }
+
+  const hit = () => {
+    clearBoard();
+    setMessage("Player hit the ball! Next player up to bat!");
   }
 
   return (
     <div className="App">
+      <div>
+        <p>{message}</p>
+      </div>
       <Scoreboard balls={balls} strikes={strikes} fouls={fouls}/>
       <Dashboard increaseBall={increaseBall} increaseStrike={increaseStrike} increaseFoul={increaseFoul} hit={hit}/>
     </div>
